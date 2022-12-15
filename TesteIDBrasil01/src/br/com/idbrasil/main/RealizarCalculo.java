486804aeda5f82595e9a70503359e556f4bfa3a2
Gustavo Boroski <gugamalheiro@gmail.com>
2022-12-14 21:33:17 -0300

    inicio do exercicio

---
+++ TesteIDBrasil01/src/br/com/idbrasil/main/RealizarCalculo.java
@@ -0,0 +1,18 @@
+package br.com.idbrasil.main;
+
+import br.com.idbrasil.model.NumerosRomanos;
+
+public class RealizarCalculo {
+
+	public static void main(String[] args) {
+
+		NumerosRomanos numeroRomano = new NumerosRomanos();
+		System.out.println(numeroRomano.getI());
+		System.out.println(numeroRomano.getV());
+		System.out.println(numeroRomano.getX());
+		System.out.println(numeroRomano.getL());
+		System.out.println(numeroRomano.getC());
+		System.out.println(numeroRomano.getD());
+		System.out.println(numeroRomano.getM());
+	}
+}
---
+++ TesteIDBrasil01/src/br/com/idbrasil/model/NumerosRomanos.java
@@ -0,0 +1,46 @@
+package br.com.idbrasil.model;
+
+public class NumerosRomanos {
+
+	
+	/*
+	 * 	private Integer I 		->      1º - PODE REPETIR 3X		|	2º - A DIREITA DO MAIOR SOMA	|	3º - A ESQUERDA DO MAIOR SUBTRAI  
+	 * 	private Integer V  		->      1º - NÃO PODE SE REPETIR 	|	2º - NÃO UTILIZA DA REGRA		|	3º - NÃO UTILIZA DA REGRA
+	 * 	private Integer X  		->      1º - PODE REPETIR 3X		|	2º - A DIREITA DO MAIOR SOMA	|	3º - A ESQUERDA DO MAIOR SUBTRAI
+	 * 	private Integer L  		->      1º - NÃO PODE SE REPETIR	|	2º - NÃO UTILIZA DA REGRA		|	3º - NÃO UTILIZA DA REGRA	
+	 * 	private Integer C  		->      1º - PODE REPETIR 3X		|	2º - A DIREITA DO MAIOR SOMA	|	3º - A ESQUERDA DO MAIOR SUBTRAI
+	 * 	private Integer D  		->      1º - NÃO PODE SE REPETIR	|	2º - NÃO UTILIZA DA REGRA		|	3º - NÃO UTILIZA DA REGRA 
+	 * 	private Integer M   	->      1º - PODE REPETIR 3X		|	2º - NÃO UTILIZA DA REGRA		|	3º - NÃO UTILIZA DA REGRA
+	 */	 
+	
+	
+	private Integer I; //Menor							 
+	private Integer V; //Maior
+	private Integer X; //Menor 
+	private Integer L; //Maior
+	private Integer C; //Menor
+	private Integer D; //Maior
+	private Integer M; //Maior
+	
+	public Integer getI() {
+		return I = 1;
+	}
+	public Integer getV() {
+		return V = 5;
+	}
+	public Integer getX() {
+		return X = 10;
+	}
+	public Integer getL() {
+		return L = 50;
+	}
+	public Integer getC() {
+		return C = 100;
+	}
+	public Integer getD() {
+		return D = 500;
+	}
+	public Integer getM() {
+		return M = 1000;
+	}
+}
