1e88d44a7a827a1dda2a27f0cf3edb62ac1acb9d
Gustavo Boroski <gugamalheiro@gmail.com>
2022-12-19 02:08:57 -0300

    implementando

--- TesteIDBrasil01/src/br/com/idbrasil/controller/QualOperacao.java
+++ TesteIDBrasil01/src/br/com/idbrasil/controller/QualOperacao.java
@@ -64,14 +64,43 @@ public class QualOperacao {
 	
 	public Integer realizarSubtracao() throws Exception {
 		NumerosRomanos nr = new NumerosRomanos();
-		Integer somatoria = 0;
-		for (int i = 0; i < listaDeNumeros.size(); i++) {
-			if (nr.getV() == listaDeNumeros.get(i) || nr.getL() == listaDeNumeros.get(i) || nr.getD() == listaDeNumeros.get(i) || nr.getM() == listaDeNumeros.get(i)) {
-				throw new Exception("Numeros adicionados não foram aceitos!");
-			} else {
-				somatoria += listaDeNumeros.get(i);
+		Integer subtracao = 0;
+		int maxI = 0;
+		int maxX = 0;
+		int maxC = 0;
+		if (listaDeNumeros.contains(nr.getI()) || listaDeNumeros.contains(nr.getX()) || listaDeNumeros.contains(nr.getC())) {
+			for (int i = 0; i < listaDeNumeros.size(); i++) {
+				if(listaDeNumeros.get(i) == nr.getI() && maxI <= 3) {
+					if(listaDeNumeros.get(i + 1) >= listaDeNumeros.get(i)) {
+						subtracao -= listaDeNumeros.get(i);
+						maxI++;
+						continue;
+					}else if(listaDeNumeros.get(i - 1) == listaDeNumeros.get(0)) {
+						
+					}
+				}
+				if(listaDeNumeros.get(i) == nr.getX() && maxX <= 3) {
+					if(listaDeNumeros.get(i + 1) >= listaDeNumeros.get(i)) {
+						subtracao -= listaDeNumeros.get(i);
+						maxX++;
+						continue;
+					}
+				}
+				if(listaDeNumeros.get(i) == nr.getC() && maxC <= 3) {
+					if(listaDeNumeros.get(i + 1) >= listaDeNumeros.get(i)) {
+						subtracao -= listaDeNumeros.get(i);
+						maxC++;
+						continue;
+					}
+				}
+				if(listaDeNumeros.get(i) != nr.getI() && listaDeNumeros.get(i) != nr.getX() && listaDeNumeros.get(i) != nr.getC()) {
+					subtracao -= listaDeNumeros.get(i);
+				}else {
+					throw new Exception("Numeros adicionados não foram aceitos!");
+				}
 			}
-		}
-		return somatoria;
+		}		
+		return subtracao;
 	}
+	
 }
